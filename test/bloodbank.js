823551f36132d1190b38f0e54934b3c3fe7c4060
shweta <shwetadarekar04@gmail.com>
2021-07-04 15:00:12 +0530

    Total 16 Test cases added

--- test/bloodbank.js
+++ test/bloodbank.js
@@ -13,7 +13,7 @@ const deploy = async (accounts) => {
     const setup = init.init(accounts);
 
     setup.bank = await init.setupBank(setup.roles.deployer);
-   // setup.donor = await init.setupBank(setup.roles.donor1);
+   //setup.bank is contract
     return setup;
 }
 
@@ -30,14 +30,15 @@ contract('Bank', async (accounts) => {
             bank1.email = "admin@bank1.com";
             bank1.contact = "1234567891";
         });
-        it("the deployer is the admin", async () => {
+        it("# the deployer is the owner", async () => {
             expect(await setup.bank.owner()).to.equal(setup.roles.deployer);
         });
-       
+
     });
+   
     context(">> NewBank", async () => {
         context("$ regsitering for the first time", async () => {
-            it("add a new bank", async () => {
+            it("# add a new bank", async () => {
                 setup.data.tx = await setup.bank.NewBank(
                     bank1.address,
                     bank1.name,
@@ -46,14 +47,22 @@ contract('Bank', async (accounts) => {
                     bank1.contact,
                     {from: setup.roles.deployer}
                 );
-
-                setup.data.bankId = await setup.bank.bankId(setup.roles.admin);
-                const response = await setup.bank.banks(setup.data.bankId);
-                expect(response.isBank.toString()).to.equal('1');
-              
             });
-           
-            it("should emit event BankRegistered", async () => {
+            
+            it("# Bank is Exist ", async () => {
+              //  setup.data.bankId = await setup.bank.bankId(setup.roles.admin)
+                const bankId = await setup.bank.bankId(setup.roles.admin);
+                const response = await setup.bank.banks(bankId);
+                expect(response.isBank.toString()).to.equal('1');
+            });
+            it("# Admin exist", async () => {
+                //  setup.data.bankId = await setup.bank.bankId(setup.roles.admin)
+                  const bankId = await setup.bank.bankId(setup.roles.admin);
+                  const response = await setup.bank.banks(bankId);
+                  expect(response.isadmin.toString()).to.equal('1');
+              });
+               
+            it("# should emit event BankRegistered", async () => {
                 await expectEvent.inTransaction(
                     setup.data.tx.tx,
                     setup.bank,
@@ -66,9 +75,12 @@ contract('Bank', async (accounts) => {
                // console.log(setup.bankId);
                 
             });
-        })
-        context("$ registering again", async () => {
-            it("reverts", async () => {
+        });
+    
+
+
+    context("$ registering again", async () => {
+            it("# Reverts", async () => {
                 await expectRevert(
                     setup.bank.NewBank(
                         bank1.address,
@@ -82,7 +94,55 @@ contract('Bank', async (accounts) => {
                 );
             });
         });
+    context("$ Deployer trying to register as a bank", async() => {
+            before("!! Setup", async () => {
+                bank1.address = setup.roles.deployer;
+                bank1.name = "Bank";
+                bank1.city = "Pune";
+                bank1.email = "admin@bank1.com";
+                bank1.contact = "1234561451";
+            });  
+            it("# Reverts", async () => {
+                await expectRevert(
+                    setup.bank.NewBank(
+                        bank1.address,
+                        bank1.name,
+                        bank1.city,
+                        bank1.email,
+                        bank1.contact,
+                        {from: setup.roles.deployer}
+                    ),
+                    "Owner:You cant be register as a bank."
+                );
+            });  
     });
+    context("$ Donor is registering a bank", async() => {
+        before("!! Setup", async () => {
+            bank1.address = setup.roles.deployer;
+            bank1.name = "Bank1";
+            bank1.city = "Pune";
+            bank1.email = "admin@bank1.com";
+            bank1.contact = "1224561451";
+        });  
+        it("# Reverts", async () => {
+            await expectRevert(
+                setup.bank.NewBank(
+                    bank1.address,
+                    bank1.name,
+                    bank1.city,
+                    bank1.email,
+                    bank1.contact,
+                    {from: setup.roles.donor1}
+                ),
+                "You don't admin have access."
+            );
+        });
+       
+      
+});
+        
+
+});
     context(">> Donor", async() => {
             before("!! Setup", async () => {
                 const bankId = await setup.bank.bankId(setup.roles.admin);
@@ -98,7 +158,7 @@ contract('Bank', async (accounts) => {
                  donor1.recentDonation = "60",
                 {from: setup.roles.donor1}
             });  
-            it("The donor is new donor", async () => {
+            it("# The donor is new donor", async () => {
                 const donorId = await setup.bank.donorId(setup.roles.donor1);
                 const response = await setup.bank.donors(donorId);
                 expect(response.isDonor.toString()).to.equal('0');
@@ -108,7 +168,7 @@ contract('Bank', async (accounts) => {
     });
     context(">> New Donor", async () => {
         context("$ regsitering donor for the first time", async () => {
-            it("add a new donor", async () => {
+            it("# add a new donor", async () => {
                 setup.data.tx = await setup.bank.RegisterDonor(
                     donor1.Id,
                     donor1.name,
@@ -121,28 +181,143 @@ contract('Bank', async (accounts) => {
                     donor1.recentDonation,
                    {from: setup.roles.donor1}
                 );
-                const donorId = await setup.bank.donorId(setup.roles.donor1);
-                const response = await setup.bank.donors(donorId);
-                expect(response.isDonor.toString()).to.equal('1');
-              
+            
             });
-            it("should emit event DonorRegistered", async () => {
- 
+            it("# should emit event DonorRegistered", async () => {
                 await expectEvent.inTransaction(
-                    
                     setup.data.tx.tx,
-                    
                     setup.bank,
                     "DonorRegistered"
                 );
                // console.log(setup.bankId);
                 
             });
-            
+            it("# isDonor is equal to 1 after registration", async () => { 
+                const donorId = await setup.bank.donorId(setup.roles.donor1);
+                const response = await setup.bank.donors(donorId);
+                expect(response.isDonor.toString()).to.equal('1');
+            });
         });
-     
-
-       
+    context("$ donor registering again", async () => {
+        it("# Reverts", async () => {
+            await expectRevert(
+                setup.bank.RegisterDonor(
+                    donor1.Id,
+                    donor1.name,
+                    donor1.city,
+                    donor1.contact,
+                    donor1.age,
+                    donor1.gender,
+                    donor1.bloodgrp,
+                    donor1.cname,
+                    donor1.recentDonation,
+                   {from: setup.roles.donor1}
+                ),
+                "Already registered"
+            );
+        });
     });
+
+    context("$ Donor registering in bank which is not exist", async() => {
+        before("!! Setup", async () => {
+            const bankId = await setup.bank.bankId(setup.roles.admin);
+                
+            donor1.Id = 4,
+            donor1.name = "DEF",
+            donor1.city = "Khopoli",
+            donor1.contact = "1234567890",
+            donor1.age = "30",
+            donor1.gender = "Female",
+            donor1.bloodgrp = "A-positive",
+            donor1.cname = "Bank",
+            donor1.recentDonation = "60",
+           {from: setup.roles.donor2}
+        });  
+        it("# Reverts", async () => {
+            await expectRevert(
+                setup.bank.RegisterDonor(
+                    donor1.Id,
+                    donor1.name,
+                    donor1.city,
+                    donor1.contact,
+                    donor1.age,
+                    donor1.gender,
+                    donor1.bloodgrp,
+                    donor1.cname,
+                    donor1.recentDonation,
+                   {from: setup.roles.donor2}
+                ),
+                "Bloodbank does not exist"
+            );
+        });
+        
+      
+});
+  context("$ False Validation : Contact", async() => {
+    before("!! Setup", async () => {
+        const bankId = await setup.bank.bankId(setup.roles.admin);     
+        donor1.Id = 1,
+        donor1.name = "DEF",
+        donor1.city = "Khopoli",
+        donor1.contact = "12345",
+        donor1.age = "30",
+        donor1.gender = "Female",
+        donor1.bloodgrp = "A-positive",
+        donor1.cname = "Bank",
+        donor1.recentDonation = "60",
+       {from: setup.roles.donor2}
+    });  
+    it("# Reverts", async () => {
+        await expectRevert(
+            setup.bank.RegisterDonor(
+                donor1.Id,
+                donor1.name,
+                donor1.city,
+                donor1.contact,
+                donor1.age,
+                donor1.gender,
+                donor1.bloodgrp,
+                donor1.cname,
+                donor1.recentDonation,
+               {from: setup.roles.donor2}
+            ),
+            "Contact No should be 10 digits."
+        );
+    }); 
+});
+context("$ False Validation: Age", async() => {
+    before("!! Setup", async () => {
+        const bankId = await setup.bank.bankId(setup.roles.admin);
+        donor1.Id = 1,
+        donor1.name = "DEF",
+        donor1.city = "Khopoli",
+        donor1.contact = "1234567894",
+        donor1.age = "3012",
+        donor1.gender = "Female",
+        donor1.bloodgrp = "A-positive",
+        donor1.cname = "Bank",
+        donor1.recentDonation = "60",
+       {from: setup.roles.donor2}
+    });  
+    it("# Reverts", async () => {
+        await expectRevert(
+            setup.bank.RegisterDonor(
+                donor1.Id,
+                donor1.name,
+                donor1.city,
+                donor1.contact,
+                donor1.age,
+                donor1.gender,
+                donor1.bloodgrp,
+                donor1.cname,
+                donor1.recentDonation,
+               {from: setup.roles.donor2}
+            ),
+            "Age should be 2 digits."
+        );
+    }); 
+  });     
+});
+     context(">> Book Slot")
 
 });
