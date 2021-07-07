981e939839b6c6f4dddc956522d9ac02291c7cc6
Nikita <you@example.com>
2021-07-08 00:22:05 +0530

    started web3

---
+++ script/eventListener.js
@@ -0,0 +1,11 @@
+const listenToBookslot = () => {
+    state.Bloodbank.events
+        .Bookslot()
+        .on("data", (event) => {
+            console.log(event);
+            alert("New slot is added");
+        })
+        .on("error", (error, data) => {
+            console.log(error, data);
+        });
+};
